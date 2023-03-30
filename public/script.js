3a14810820fca723c99b103561eb2aca69fdc3c2
gambadio <ricardo.kupper@outlook.com>
2023-03-30 02:27:03 +0100

    it's working

--- public/script.js
+++ public/script.js
@@ -1,13 +1,11 @@
-document.getElementById('send-button').addEventListener('click', async () => {
-    const userInput = document.getElementById('user-input');
-    const chatMessages = document.getElementById('chat-messages');
+document.getElementById('send-button').addEventListener('click', async () => { const userInput = document.getElementById('user-input'); const chatMessages = document.getElementById('chat-messages');
 
     const userMessage = userInput.value;
     if (userMessage.trim() === '') return;
-
+    
     // Display the user's message
     chatMessages.innerHTML += `<div class="user-message">${userMessage}</div>`;
-
+    
     // Send the message to the backend and get the GPT response
     try {
         const response = await fetch('/api/chat', {
@@ -17,17 +15,17 @@ document.getElementById('send-button').addEventListener('click', async () => {
             },
             body: JSON.stringify({ message: userMessage }),
         });
-
+    
         const data = await response.json();
         const gptMessage = data.message;
-
+    
         // Display the GPT response
         chatMessages.innerHTML += `<div class="gpt-message">${gptMessage}</div>`;
     } catch (error) {
         console.error(error);
     }
-
+    
     // Clear the user input and scroll to the bottom of the chat
     userInput.value = '';
     chatMessages.scrollTop = chatMessages.scrollHeight;
-});
+    });
--- server.js
+++ server.js
@@ -1,40 +1,39 @@
-const express = require('express');
-const { OpenAIApi, Configuration } = require('openai');
-const bodyParser = require('body-parser');
+const express = require('express'); 
+const { OpenAIApi, Configuration } = require('openai'); 
+const bodyParser = require('body-parser'); 
+const path = require('path');
 
-const app = express();
-app.use(bodyParser.json());
+const app = express(); app.use(bodyParser.json()); app.use(express.static(path.join(__dirname, 'public')));
 
-const OPENAI_API_KEY = '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';
-const configuration = new Configuration({ apiKey: OPENAI_API_KEY });
-const openai = new OpenAIApi(configuration);
+const OPENAI_API_KEY = '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'; const configuration = new Configuration({ apiKey: OPENAI_API_KEY }); const openai = new OpenAIApi(configuration);
 
-app.post('/api/chat', async (req, res) => {
-    const userMessage = req.body.message;
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 
-    try {
-        const response = await openai.createChatCompletion({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'user',
-                    content: userMessage,
-                },
-            ],
-            temperature: 0.7,
-            max_tokens: 100,
-            n: 1,
-        });
+app.post('/api/chat', async (req, res) => { const userMessage = req.body.message;
 
-        const gptResponse = response.data.choices[0].message.content.trim();
-        res.json({ message: gptResponse });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error processing the request' });
-    }
+try {
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: userMessage,
+            },
+        ],
+        temperature: 0.7,
+        max_tokens: 100,
+        n: 1,
+    });
+
+    const gptResponse = response.data.choices[0].message.content.trim();
+    res.json({ message: gptResponse });
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error processing the request' });
+}
 });
-
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
